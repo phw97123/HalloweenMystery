66546af6ee92a1993f9a37930f519f713d9ac46d
김대열 <ajduf7180@gmail.com>
2023-09-25 18:56:17 +0900

    [Feat] RotateArm 기능 구현 (-대열)

--- Assets/Scripts/Entities/PlayerCharacterController.cs
+++ Assets/Scripts/Entities/PlayerCharacterController.cs
@@ -1,4 +1,5 @@
 using Entites;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,16 +7,28 @@ namespace Entities
 {
     public class PlayerCharacterController : EntityController
     {
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
         public void OnMove(InputValue input)
         {
             Vector2 dir = input.Get<Vector2>();
-            CallMove(dir);
+            CallMove(dir.normalized);
         }
 
         public void OnLook(InputValue input)
         {
-            Vector2 dir = input.Get<Vector2>();
-            CallMove(dir);
+            Vector2 screenPoint = input.Get<Vector2>();
+            Debug.Log($"screenPosition is {screenPoint}");
+            Vector3 worldPoint = _camera.ScreenToWorldPoint(screenPoint).normalized;
+            Debug.Log($"worldPoint is {worldPoint}");
+            Vector2 dir = worldPoint - transform.position;
+            Debug.Log($"dir is {dir}");
+            CallLook(dir);
         }
 
         public void OnAttack(InputValue value)
