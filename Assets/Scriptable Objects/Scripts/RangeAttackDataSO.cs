0e78d611580c2126d7e61afd301472a254471b1b
김대열 <ajduf7180@gmail.com>
2023-09-25 22:31:56 +0900

    [Add] 공격 데이터 SO 추가 (-대열)

---
+++ Assets/Scriptable Objects/Scripts/RangeAttackDataSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Default Range Attack Data", menuName = "Component/Attack/Range", order = 0)]
+public class RangeAttackDataSO : AttackDataSO
+{
+    [Header("Range Attack Info")] public string bulletTag;
+    public float rangeOfAttack;
+    public float projectilesPerAttack;
+    public float anglePerShot;
+    public float piercingCount;
+}
