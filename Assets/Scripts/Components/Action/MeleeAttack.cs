06d0289bf683403789fdcc12eb27fda2022320f2
김대열 <ajduf7180@gmail.com>
2023-09-26 14:44:06 +0900

    [Feat] 근접 전투시 연속공격 기능 추가 (-대열)

--- Assets/Scripts/Components/Action/MeleeAttack.cs
+++ Assets/Scripts/Components/Action/MeleeAttack.cs
@@ -11,18 +11,18 @@ namespace Components.Action
         private EntityController _controller;
         private StatsHandler _stats;
         [SerializeField] private GameObject testPrefab;
-
         [SerializeField] private Transform attackPosition;
 
-        //todo animatior
+        //todo animator
         private Vector2 _direction = Vector2.zero;
         private float _timeSinceLastAttack = float.MaxValue;
         private bool _isAttacking = false;
+        private int _currentAttackCount = 0;
 
         private void Awake()
         {
-            _controller = GetComponent<EntityController>();
-            _stats = GetComponent<StatsHandler>();
+            _controller = GetComponentInParent<EntityController>();
+            _stats = GetComponentInParent<StatsHandler>();
             Debug.Assert(_controller != null);
             Debug.Assert(_stats != null);
         }
@@ -36,41 +36,77 @@ namespace Components.Action
         private void Update()
         {
             _timeSinceLastAttack += Time.deltaTime;
-            
-            if (_timeSinceLastAttack <= _stats.CurrentStats.attackData.delay)
-            {
-                _isAttacking = false;
-                return;
-            }
-
             if (_isAttacking)
             {
-                _timeSinceLastAttack = 0f;
-                _isAttacking = false;
-
                 MeleeAttackDataSO meleeAttack = _stats.CurrentStats.attackData as MeleeAttackDataSO;
                 if (meleeAttack == null)
                 {
                     return;
                 }
 
+                _timeSinceLastAttack = 0f;
+                _isAttacking = false;
                 HandleAttack(meleeAttack, attackPosition.position, _direction);
             }
         }
 
         private void Attack()
         {
-            _isAttacking = true;
+            MeleeAttackDataSO meleeAttack = _stats.CurrentStats.attackData as MeleeAttackDataSO;
+            if (meleeAttack == null) return;
+
+
+            if (_timeSinceLastAttack >= meleeAttack.delay)
+            {
+                _currentAttackCount = 1;
+                _isAttacking = true;
+            }
+            else if (_currentAttackCount < meleeAttack.attackCount)
+            {
+                _isAttacking = true;
+                _currentAttackCount++;
+            }
         }
 
         private void Aim(Vector2 direction) => _direction = direction;
 
         private void HandleAttack(MeleeAttackDataSO meleeAttack, Vector2 spawnPosition, Vector2 direction)
         {
-            //todo apply meleeAttack, direction
+            //todo divide SwingAttack, LungeAttack
             GameObject go = Instantiate(testPrefab, spawnPosition, Quaternion.identity);
             MeleeAttackController controller = go.GetComponent<MeleeAttackController>();
-            controller.Initialize(meleeAttack, direction);
+            if (meleeAttack.arc > 0)
+            {
+                if (_currentAttackCount % 3 == 0)
+                {
+                    //Lunge Attack
+                    controller.Initialize(
+                        meleeAttack: meleeAttack,
+                        startPosition: spawnPosition,
+                        direction: direction,
+                        isReversed: true,
+                        isLunge: true);
+                }
+                else if ((_currentAttackCount & 1) == 1)
+                {
+                    float rad = meleeAttack.arc * Mathf.Deg2Rad;
+                    float x = Mathf.Cos(rad);
+                    float y = Mathf.Sin(rad);
+                    controller.Initialize(
+                        meleeAttack: meleeAttack,
+                        startPosition: spawnPosition,
+                        direction: direction + new Vector2(x, y),
+                        isReversed: true);
+                }
+                else
+                {
+                    controller.Initialize(meleeAttack, spawnPosition, direction);
+                }
+            }
+            else
+            {
+                controller.Initialize(meleeAttack, spawnPosition, direction);
+            }
         }
     }
 }
