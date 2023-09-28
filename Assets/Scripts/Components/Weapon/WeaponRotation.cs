887b3ef8c05fbba3ae6dcfa1f9e93ea7572a19af
김대열 <ajduf7180@gmail.com>
2023-09-28 22:56:13 +0900

    [Feat] 무기 회전 (-대열)

--- Assets/Scripts/Components/Weapon/WeaponRotation.cs
+++ Assets/Scripts/Components/Weapon/WeaponRotation.cs
@@ -1,18 +1,36 @@
 using Entites;
 using System;
+using System.Linq;
 using UnityEngine;
+using Utils;
 
 namespace Components.Action
 {
     public class WeaponRotation : MonoBehaviour
     {
         private EntityController _controller;
+        private Transform _armPivot;
         private SpriteRenderer _renderer;
 
         private void Awake()
         {
+            _armPivot = transform;
             _controller = GetComponentInParent<EntityController>();
-            _renderer = GetComponentInParent<SpriteRenderer>();
+            _renderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        private void Start()
+        {
+            if (_controller == null) { return; }
+
+            _controller.OnLookEvent += RotateWeapon;
+        }
+
+        private void RotateWeapon(Vector2 dir)
+        {
+            float angleDegree = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            _armPivot.rotation = Quaternion.Euler(0, 0, angleDegree);
+            _renderer.flipY = Mathf.Abs(angleDegree) >= 90f;
         }
     }
 }
