4d835d35a91952b2cf9357441e644a230ce2c426
phw97123 <phw97123@naver.com>
2023-10-02 21:57:30 +0900

    [REFACTOR] SoundManager 리팩토링 및 BGM 수정
    BGM 로직 리팩토링

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -29,9 +29,9 @@ public class SoundManager : MonoBehaviour
     }
 
     private AudioClip _soundChangedClip;
-    [SerializeField] [Range(0f, 1f)] private float soundEffectVolume;
-    [SerializeField] [Range(0f, 1f)] private float soundEffectPitchVariance;
-    [SerializeField] [Range(0f, 1f)] private float musicVolume;
+    [SerializeField][Range(0f, 1f)] private float soundEffectVolume;
+    [SerializeField][Range(0f, 1f)] private float soundEffectPitchVariance;
+    [SerializeField][Range(0f, 1f)] private float musicVolume;
 
     private ObjectPool _objectPool;
     private List<Poolable> _prefabs;
@@ -40,10 +40,12 @@ public class SoundManager : MonoBehaviour
 
     private const string START_SCENE = "StartScene";
     private const string INTRO_SCENE = "TownScene";
-    private const string STAGE_SCENE = "RoomContent";
+    private const string STAGE1_SCENE = "Stage1";
+    private const string STAGE2_SCENE = "Stage2";
+    private const string STAGE3_SCENE = "Stage3";
     private const string DEMO_SCENE = "DemoScene";
-    private const string ROOM_SCENE = "RoomScene";
-    private const string GAMEENDING_SCENE = "";
+    //private const string ROOM_SCENE = "RoomScene";
+    private const string GAMEENDING_SCENE = "EndingScene";
 
     private AudioClip _musicClip;
 
@@ -78,7 +80,8 @@ public class SoundManager : MonoBehaviour
             _prefabs.Add(p);
         }
 
-        _objectPool.Initialize(_prefabs); 
+        _objectPool.Initialize(_prefabs);
+
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
@@ -100,43 +103,33 @@ public class SoundManager : MonoBehaviour
     {
         _musicClip = null;
 
-        if (sceneName == START_SCENE)
+        switch (sceneName)
         {
-            _musicClip = Resources.Load<AudioClip>("Sound/TitleBGM");
-        }
-        else if (sceneName == INTRO_SCENE)
-        {
-            _musicClip = Resources.Load<AudioClip>("Sound/IntroBGM");
-        }
-        else if (sceneName == STAGE_SCENE)
-        {
-            //TODO
-
-            //if (stage == 1)
-            //{
-            //    _musicClip = Resources.Load<AudioClip>("Sound/Stage1");
-            //}
-            //else if (stage == 2)
-            //{
-            //    _musicClip = Resources.Load<AudioClip>("Sound/Stage2");
-            //}
-            //else
-            //{
-            //    _musicClip = Resources.Load<AudioClip>("Sound/Stage3");
-            //}
-        }
-        else if (sceneName == DEMO_SCENE)
-        {
-            _musicClip = Resources.Load<AudioClip>("Sound/Stage1");
-        }
-        else if (sceneName == GAMEENDING_SCENE)
-        {
-            //TODO:EndingScene, bgm add
+            case START_SCENE:
+                _musicClip = Resources.Load<AudioClip>("Sound/TitleBGM");
+                break;
+            case INTRO_SCENE:
+                _musicClip = Resources.Load<AudioClip>("Sound/IntroBGM");
+                break;
+            case STAGE1_SCENE:
+                _musicClip = Resources.Load<AudioClip>("Sound/Stage1");
+                break;
+            case STAGE2_SCENE:
+                _musicClip = Resources.Load<AudioClip>("Sound/Stage2");
+                break;
+            case STAGE3_SCENE:
+                _musicClip = Resources.Load<AudioClip>("Sound/Stage3");
+                break;
+            case DEMO_SCENE:
+                _musicClip = Resources.Load<AudioClip>("Sound/Stage1");
+                break;
+            case GAMEENDING_SCENE:
+                _musicClip = Resources.Load<AudioClip>("Sound/GameOver");
+                break;
         }
 
         ChangeBGM(_musicClip);
     }
-
     private static void ChangeBGM(AudioClip music)
     {
         Instance._musicAudioSource.Stop();
